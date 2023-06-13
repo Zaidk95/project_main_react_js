5796a64968ab0dfa1f002ff77888802518d1ef61
Zaidk95 <zaidsh845@gmail.com>
2023-06-13 23:36:44 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+const al = () => alert("wow")
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. wow ZZ
+        </p>
+        <button
+          className="App-link"
+          onClick={al}
+        >
+          Learn React zozo
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
